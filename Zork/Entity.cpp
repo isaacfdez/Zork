fd7950d8ec64f2105f68b3b9551612e56b0d6477
isaac fernandez fontao <isaacfdez94@gmail.com>
2020-05-29 16:02:16 +0200

    Implementation of Entity Class

--- Zork/Entity.cpp
+++ Zork/Entity.cpp
@@ -1,1 +1,65 @@
 #include "Entity.h"
+
+Entity::Entity(const char* name, const char* desc, Entity* parent = NULL) : name(name), desc(desc), parent(parent)
+{
+	type = entity;
+
+	if (parent != NULL)
+	{
+		parent->gameElements.push_back(this);
+	}
+}
+
+Entity::~Entity()
+{
+}
+
+void Entity::updateStatus()
+{
+}
+
+
+void Entity::LookAt() const
+{
+	cout << name << "\n";
+	cout << desc << "\n";
+}
+
+void Entity::ChangeParent(Entity* newParent)
+{
+	if (parent != NULL) 
+	{
+		parent->gameElements.remove(this);
+	}
+
+	parent = newParent;
+
+	if (parent != NULL)
+	{
+		parent->gameElements.push_back(this);
+	}
+}
+
+bool Entity::exists(Entity* otherEntity) const
+{
+	for (list<Entity*>::const_iterator gameElement = gameElements.begin(); gameElement != gameElements.end(); gameElement++)
+	{
+		if ((*gameElement) == otherEntity)
+		{
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+Entity* Entity::getElement(const string& name, Type type) const
+{
+	for (list<Entity*>::const_iterator gameElement = gameElements.begin(); gameElement != gameElements.end(); gameElement++)
+	{
+		if ((*gameElement)->type == type && _stricmp((*gameElement)->name.c_str(), name.c_str()) == 0)
+		{
+			return *gameElement;
+		}
+	}
+}
--- Zork/Entity.h
+++ Zork/Entity.h
@@ -1,5 +1,33 @@
-#pragma once
-class Entity
+#ifndef __Entity__
+#define __Entity__
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+enum Type //enum to set the type for the diferent classes inheriting Entity
 {
+	creature, entity, exit, item, player, room
 };
 
+class Entity //class that defines common variables and functions for all game elements
+{
+public: //variables
+	Type type; //type of the Entity
+	std::string name; //name of the Entity
+	std::string desc; //description of the Entity
+	Entity* parent; //Entity's parent, if it has one
+	list<Entity*> gameElements; //List of Entitys contained in this Entity
+
+public: //functions
+	Entity(const char* name, const char* desc, Entity* parent); //constructor
+	virtual ~Entity(); //destructor
+	virtual void updateStatus(); //function that updates the status of the Entity
+	virtual void LookAt() const; //function that describes the Entity
+	void ChangeParent(Entity* newParent); //function that changes this Entity's parent
+	bool exists(Entity* otherEntity) const; //function that checks if another Entity exists in this Entity
+	Entity* getElement(const string& name, Type type) const; //function that gets target Entity comparing its name and type
+};
+
+#endif
